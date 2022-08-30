0d26b2c89556de0c6aed8fa9acf4424c4dbae13d
H0UND <35621838+H0UND@users.noreply.github.com>
2022-08-30 20:53:54 +0700

    Update Text

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello word");
+        Debug.Log("Good bye");
     }
 
     // Update is called once per frame
