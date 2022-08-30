421320cd32e1eaf556d08a595fbc071dfd7c688e
H0UND <35621838+H0UND@users.noreply.github.com>
2022-08-30 20:58:17 +0700

    New method

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,18 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Hello word");
+
+        SayHello();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SayHello()
     {
-        
+        Debug.Log("Hello all!!!");
     }
 }
